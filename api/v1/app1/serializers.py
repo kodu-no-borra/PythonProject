239f6d7dd4b29c603ac59ec7d9a44de5f137ecfb
kodu-no-borra <d4wyd0ffden@yandex.ru>
2025-11-10 19:10:36 +0400

    REFACTORING: Add id in response

--- api/v1/app1/serializers.py
+++ api/v1/app1/serializers.py
@@ -5,7 +5,7 @@ from task.models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ('title', 'description', 'status', 'created_at', 'updated_at')
+        fields = ('title', 'description', 'status', 'created_at', 'updated_at', 'id')
         read_only_fields = ('created_at', 'updated_at')
 
 
